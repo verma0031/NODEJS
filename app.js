39d089cc923601fc6e5405405974caa201dc06c7
verma0031 <peeyushverma888@gmial.com>
2023-03-26 19:07:23 +0530

    added functionality of opening page according to url

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
 const http = require('http');
+const fs = require('fs');
 
 const port=4000;
 
 const server = http.createServer( ( req , res) =>{
-    res.write("Hello my name is PEEYUSH VERMA" , 
-    console.log("Hello my name is PEEYUSH VERMA"))
-    res.end()
+    console.log(req);
+    res.setHeader('Content-type' , 'text/html')
+    if(req.url === '/home'){
+        fs.readFile("./home.html" , (err , page)=>{
+            if(err)
+            throw err;
+            res.write(page);
+            res.end();
+        })
+    }
+    if(req.url === '/about'){
+        fs.readFile("./about.html" , (err , page)=>{
+            if(err)
+            throw err;
+            res.write(page);
+            res.end();
+        })
+    }
+    if(req.url === '/node'){
+        fs.readFile("./node.html" , (err , page)=>{
+            if(err)
+            throw err;
+            res.write(page);
+            res.end();
+        })
+    }
 })
 
 server.listen(port);
