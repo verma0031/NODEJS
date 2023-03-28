dfa8d055b09f9c81158c68e767902832ef468816
verma0031 <peeyushverma888@gmial.com>
2023-03-28 15:08:17 +0530

    implemented rouitng request in expressjs also added  error404 page

--- app.js
+++ app.js
@@ -7,30 +7,21 @@ const port=4000;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended : true}));
+const adminRoutes = require('./routes/admin');
+
+const shopRoutes = require('./routes/shop');
 
 
-app.use('/add-Product' , (req , res , next)=>{
-    res.send(`
-    <form action = "/product" method = "POST">
-        <input type ="text" name = "title"><br/>
-        <input type ="text" name = "size">
-        <button type = "submit">Add Product</button>
+app.use(bodyParser.urlencoded({extended : false}));
 
-    </form>`);
+
+app.use(shopRoutes);
+
+app.use('/admin' , adminRoutes);
+
+app.use((req , res , next) =>{
+    res.status(404).send('<h1>Page Not Found</h1>')
 });
-
-app.post('/product' , (req , res , next) =>{
-
-    console.log(req.body);
-    res.redirect('/');
-});
-
-app.use('/' , (req , res , next)=>{
-    
-    res.send('<h1>Hello form expressJS</h1>');
-});
-
 
 
 app.listen(port);
